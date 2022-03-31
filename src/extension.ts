57d63a2ecf90fdc41300630ff04c0005186f25db
SynicalMC <70176240+SynicalMC@users.noreply.github.com>
2022-03-31 14:56:00 -0400

    first commit

---
+++ src/extension.ts
@@ -0,0 +1,167 @@
+// Import libraries
+import * as fs from 'fs';
+import * as vscode from 'vscode';
+
+// Runs on extension activation
+export function activate(context: vscode.ExtensionContext) {
+
+	// Registers 'Display NFP Image' Command
+	context.subscriptions.push(vscode.commands.registerCommand('nfp-images.dispNFP', () => {
+		let file = vscode.window.activeTextEditor?.document.fileName;
+		let fileExt = getFileExt(file);
+
+		if (fileExt === "nfp") {
+			const panel = vscode.window.createWebviewPanel(
+				'nfpImageView', // Identifies the type of the webview. Used internally
+				'NFP Image', // Title of the panel displayed to the user
+				vscode.ViewColumn.One, // Editor column to show the new webview panel in.
+				{} // Webview options. More on these later.
+			);
+
+			let image = parseImage(file);
+			panel.webview.html = getWebviewContent(image);
+		} else {
+			vscode.window.showErrorMessage("This file is not in NFP format!");
+		}
+	}));
+}
+
+// Gets file extension
+// Ex. file.TXT => txt
+function getFileExt(filename: any) {
+	let extension: string = filename.split('.').pop();
+	extension.toLowerCase();
+	return extension;
+}
+
+// Parses image into html ids
+function parseImage(file: any) {
+	let image: Array<string> = [];
+	file = fs.readFileSync(file, 'utf8');
+
+	for (let i = 0; i < file.length; i++) {
+		let char = file.charAt(i);
+		
+		switch (char){
+			case '\n':
+				image.push('newline');
+				break;
+			case ' ':
+				image.push('blank');
+				break;
+			default:
+				image.push(char);
+			
+		}
+	}
+	return image;
+}
+
+// Returns HTML content to display.
+function getWebviewContent(image: Array<string>) {
+	let body: string = `<div class="container">`;
+	for (let i = 0; i < image.length; i++) {
+		let left = i * 10;
+		if(image[i] === '\n'){
+			image[i] = 'none';
+		}
+		body = body + `<div class="pixel" id="${image[i]}" style="left:${left};">` + '\n';
+	}
+	body = body + '</div>';
+
+	let html = `
+		<!DOCTYPE html>
+		<html lang="en">
+			<head>
+			<meta charset="UTF-8">
+			<meta name="viewport" content="width=device-width, initial-scale=1.0">
+			<title>NFP Image</title>
+			</head>
+			<style>
+				.container {
+					width: 100%;
+					height: 100%;
+				}
+
+				.pixel {
+					width: 10px;
+					height: 20px;
+				}
+
+				#0 {
+					background-color: white;
+				}
+
+				#1 {
+					background-color: orange;
+				}
+
+				#2 {
+					background-color: magenta;
+				}
+
+				#3 {
+					background-color: lightblue;
+				}
+
+				#4 {
+					background-color: yellow;
+				}
+
+				#5 {
+					background-color: lime;
+				}
+
+				#6 {
+					background-color: pink;
+				}
+				
+				#7 {
+					background-color: gray;
+				}
+
+				#8 {
+					background-color: lightgray;
+				}
+
+				#9 {
+					background-color: cyan;
+				}
+
+				#a {
+					background-color: purple;
+				}
+
+				#b {
+					background-color: blue;
+				}
+
+				#c {
+					background-color: brown;
+				}
+
+				#d {
+					background-color: green;
+				}
+
+				#e {
+					background-color: red;
+				}
+
+				#f {
+					background-color: black;
+				}
+
+				#blank {
+					
+				}
+
+			</style>
+			<body>
+				${body}
+			</body>
+		</html>`;
+
+	console.log(html);
+	return html;
+}
